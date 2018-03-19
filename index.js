de50385703ef0fb39e0e058ee1f55d3241cf9b88
Jon Winton <winton.jon@gmail.com>
2018-03-19 10:12:02 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports.getIndices = require('./lib/getIndices');
+module.exports.getSchemaPath = require('./lib/getSchemaPath');
+module.exports.getComponentPath = require('./lib/getComponentPath');
+module.exports.getComponents = require('./lib/getComponents');

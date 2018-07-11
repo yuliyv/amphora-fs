86ebe6a2ee4edb98165de22be547cd46129c3e82
Jon Winton <winton.jon@gmail.com>
2018-07-11 10:07:11 -0400

    adding in some layout methods

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ module.exports.getIndices = require('./lib/getIndices');
 module.exports.getSchemaPath = require('./lib/getSchemaPath');
 module.exports.getComponentPath = require('./lib/getComponentPath');
 module.exports.getComponents = require('./lib/getComponents');
+module.exports.getLayouts = require('./lib/getLayouts');
+module.exports.getLayoutPath = require('./lib/getLayoutPath');
 module.exports.getComponentName = require('./lib/getComponentName');
 module.exports.readFilePromise = require('./lib/readFilePromise');
